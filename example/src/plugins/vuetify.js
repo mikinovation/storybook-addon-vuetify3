cd0dcf0cb60845503cadf27f647d45e382dfbeca
mikinovation <maneuver2472@gmail.com>
2022-04-30 22:59:20 +0800

    fix primary color

--- example/src/plugins/vuetify.js
+++ example/src/plugins/vuetify.js
@@ -12,7 +12,7 @@ export const myCustomLightTheme = {
   colors: {
     background: '#FFFFFF',
     surface: '#FFFFFF',
-    primary: '#123456',
+    primary: '#6200EE',
     'primary-darken-1': '#3700B3',
     secondary: '#03DAC6',
     'secondary-darken-1': '#018786',
